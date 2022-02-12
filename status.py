36bf24c0222c9c0579e1987030a36c44897beec0
Utkarsh Giri <utkarshgiri@Utkarshs-MacBook-Pro.local>
2022-02-12 00:31:01 -0600

    .

---
+++ status.py
@@ -0,0 +1,27 @@
+#!/Users/utkarshgiri/miniforge3/bin/python
+
+
+''' A simple script to set slack status based on your whether you are connected to home or office network. 
+It can be set to run automatically whenever your network connection is reset '''
+
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+client = WebClient(token=os.environ['SLACK_TOKEN'])
+
+result = os.popen('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I').read()
+
+for line in result.split('\n'):
+    if line.strip()[:4] == 'SSID':
+        ssid = line.strip()[5:].strip()
+
+statuses = {
+                os.environ['HOME_NETWORK']: 'Working from Home',
+                os.environ['OFFICE_NETWORK']: '@Chamberlin'
+           }
+
+try:
+    response = client.users_profile_set(profile={"status_text": statuses[ssid], "status_emoji": ":mountain_railway:"})
+except SlackApiError as e:
+    pass
